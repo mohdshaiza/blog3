a45d4a598b699fa14aacec52c8faef8fbe23b71c
Mohd Shaiza <mohdshaiza@gmail.com>
2020-05-12 11:01:59 +0800

    Create Blade File.

--- resources/views/adminHome.blade.php
+++ resources/views/adminHome.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    Your are normal user.
+                    You are Admin.
                 </div>
             </div>
         </div>
