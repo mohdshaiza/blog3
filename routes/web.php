54e7d01e41b4517e0524d387fa832eba91924454
Mohd Shaiza <mohdshaiza@gmail.com>
2020-05-12 10:57:10 +0800

    Create Route.

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('admin/home', 'HomeController@adminHome')->name('admin.home')->middleware('is_admin');
